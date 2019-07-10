ad9c5153645bf2c8a3b554fef035be8df76c2ae0
taiyang1994 <1209450705@qq.com>
2019-07-10 16:46:44 +0800

    merge pull request, add action-sheet
    自定义的action sheet，主要对原来的组件进行了样式上的修改

--- components/action-sheet/action-sheet.vue
+++ components/action-sheet/action-sheet.vue
@@ -1,22 +1,24 @@
-<template>
+﻿<template>
   <div class="md-action-sheet">
     <md-popup
       v-model="isActionSheetShow"
+      lighter-mask
       position="bottom"
       prevent-scroll
       @show="$_onShow"
       @hide="$_onHide"
     >
       <div class="md-action-sheet-content">
-        <header class="md-action-sheet-header" v-if="title">{{ title }}</header>
+        <header v-if="title" class="md-action-sheet-header">{{title}}</header>
         <ul class="md-action-sheet-list">
           <template v-for="(item, index) in options">
             <li
               :key="index"
               :class="{
                 'active': index === clickIndex,
-                'disabled': index=== invalidIndex,
-                'md-action-sheet-item': true
+                'disabled': index === invalidIndex,
+                'md-action-sheet-item': true,
+                'warning': item.type === 'warning'
               }"
               @click="$_onSelect(item, index)"
             >
@@ -25,155 +27,153 @@
               </div>
             </li>
           </template>
-          <li class="md-action-sheet-cancel" @click="$_onCancel">{{ cancelText }}</li>
+          <li
+            :class="['md-action-sheet-cancel',{'global-is-iphone-x': isIphoneX}]"
+            @click="$_onCancel"
+          >取&#12288;消</li>
         </ul>
       </div>
     </md-popup>
   </div>
 </template>
 
-<script>import Popup from '../popup'
-import {inArray} from '../_util'
+<script>
+import Popup from '../popup';
+import {inArray} from '../_util';
 
 export default {
   name: 'md-action-sheet',
-
   components: {
-    [Popup.name]: Popup,
+    [Popup.name]: Popup
   },
-
   props: {
     value: {
       type: Boolean,
-      default: false,
+      default: false
     },
     title: {
       type: String,
-      default: '',
+      default: ''
     },
     options: {
       type: Array,
       default() {
-        return []
-      },
+        return [];
+      }
     },
     defaultIndex: {
       type: Number,
-      default: -1,
+      default: -1
     },
     invalidIndex: {
       type: Number,
-      default: -1,
+      default: -1
     },
     cancelText: {
       type: String,
-      default: '取消',
+      default: '取消'
     },
+    isIphoneX: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'normal'
+    }
   },
-
   data() {
     return {
       isActionSheetShow: this.value,
       clickIndex: -1,
-      scroller: '',
-    }
+      scroller: ''
+    };
   },
-
   watch: {
     value(newVal) {
-      this.isActionSheetShow = newVal
-    },
+      this.isActionSheetShow = newVal;
+    }
   },
-
   created() {
-    this.clickIndex = this.defaultIndex
   },
-
   methods: {
-    // MARK: events handler, 如 $_onButtonClick
     $_onShow() {
-      this.$emit('show')
+      this.$emit('show');
     },
     $_onHide() {
-      this.$emit('hide')
-      this.$_hideSheet()
+      this.$emit('hide');
+      this.$_hideSheet();
     },
     $_onSelect(item, index) {
       if (index === this.invalidIndex || inArray(this.invalidIndex, index)) {
-        return
+        return;
       }
-      this.clickIndex = index
-      this.$emit('selected', item)
-      this.$_hideSheet()
+      this.clickIndex = index;
+      this.$emit('selected', item);
+      this.$_hideSheet();
     },
     $_onCancel() {
-      this.$emit('cancel')
-      this.$_hideSheet()
+      this.$emit('cancel');
+      this.$_hideSheet();
     },
     $_hideSheet() {
-      this.isActionSheetShow = false
-      this.$emit('input', false)
-    },
-  },
-}
-</script>
+      this.isActionSheetShow = false;
+      this.clickIndex = -1;
+      this.$emit('input', false);
+    }
+  }
+};
+</script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+.global-is-iphone-x
+  margin-bottom 24px
+.warning
+  color #F12701
 .md-action-sheet
-  color action-sheet-color
   -webkit-font-smoothing antialiased
-  .md-popup
-    z-index action-sheet-zindex
+.md-popup
+  z-index action-sheet-zindex
   .md-popup-box
-    background-color color-bg-base
-
+    background-color #fff
 .md-action-sheet-content
   position relative
   width 100%
-  font-size action-sheet-font-size
-  background action-sheet-bg
+  font-size 18px
   text-align center
   overflow auto
-
+  background #ffffff
 .md-action-sheet-header
   position relative
-  vertical-height(action-sheet-height)
-  hairline(bottom, color-border-base)
-  word-ellipsis()
-  overflow visible
-
+  height 50px
+  display flex
+  align-items center
+  justify-content center
+  font-size 13px
+  color #898989
+  padding 0 32px
+  border-bottom 1px solid #F4F4F4
 .md-action-sheet-item
   position relative
-  vertical-height(action-sheet-height)
-  padding 0 action-sheet-padding-h
   box-sizing border-box
-  font-size action-sheet-font-size
-  transition background-color .3s
+  transition background-color 0.3s
   -webkit-user-select none
+  height 50px
+  line-height 50px
   &.active
-    color action-sheet-color-highlight
+    background-color #F4F4F4
   &.disabled .md-action-sheet-item-section
-    opacity action-sheet-disabled-opacity
-  &:first-of-type
-    .md-action-sheet-item-wrapper:after
-      display none
+    opacity 0.3
   &:active
-    background-color color-bg-tap
+    background-color #F4F4F4
     &.disabled
       background-color transparent
-
-.md-action-sheet-item-wrapper
-  position relative
-  hairline(top, color-border-base)
-
 .md-action-sheet-cancel
-  height 132px
-  line-height 120px
-  color action-sheet-color-cancel
-  font-weight font-weight-medium
+  height 56px
+  line-height 50px
   &::before
     display block
     content ''
-    height 12px
-    background action-sheet-cancel-gap-bg
+    height 6px
+    background #f9fafb
 </style>
