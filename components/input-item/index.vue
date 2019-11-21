5fe59cc83b36d56e3b183ea3cda976b6d04f43f6
taiyang1994 <1209450705@qq.com>
2019-11-21 19:24:14 +0800

    input基本框

--- components/input-item/index.vue
+++ components/input-item/index.vue
@@ -1,37 +1,35 @@
 <template>
+<div class="content-wraper" :class="{'line-bottom':size!='large'}" @click="$_onClick">
+  <div style="height:20px" v-if="size=='large'">
+    <i class="unit-money" v-show="unit">{{unit}}</i>
+  </div>
   <md-field-item
     class="md-input-item"
     :class="[
-      isHighlight ? 'is-highlight' : '',
-      isTitleLatent ? 'is-title-latent' : '',
-      isInputActive ? 'is-active' : '',
-      isInputFocus ? 'is-focus' : '',
       isInputError() ? 'is-error' : '',
       isInputBrief() && !isInputError() ? 'with-brief' : '',
       isDisabled ? 'is-disabled': '',
-      isAmount ? 'is-amount': '',
       clearable ? 'is-clear' : '',
+      isAmount ? 'is-amount': '',
       inputEnv,
       align,
-      size
-    ]"
-    :title="title"
-    :solid="solid && !isTitleLatent"
+      size,
+      type
+    ]" 
+    :label="label"
+    :addon="addon"
+    :imgSrc="imgSrc"
   >
     <template slot="left">
       <slot name="left"></slot>
     </template>
-    <!-- ------------ -->
-    <!--    INPUT     -->
-    <!-- ------------ -->
-    <!-- Native Input -->
-    <template v-if="!isVirtualKeyboard">
+    <template>
       <input
         class="md-input-item-input"
         :type="inputType"
         :name="name"
         v-model="inputBindValue"
-        :placeholder="inputPlaceholder"
+        :placeholder="placeholder"
         :disabled="isDisabled"
         :readonly="readonly"
         :maxlength="isFormative ? '' : maxlength"
@@ -42,92 +40,52 @@
         @keydown="$_onKeydown"
         @input="$_onInput"
       />
-    </template>
-    <!-- Fake Input -->
-    <template v-else>
-      <div
-        class="md-input-item-fake"
-        :class="{
-          'is-focus': isInputFocus,
-          'disabled': isDisabled,
-          'readonly': readonly
-        }"
-        @click="$_onFakeInputClick"
-      >
-        <span v-text="inputValue"></span>
-        <span
-          class="md-input-item-fake-placeholder"
-          v-if="inputValue === '' && inputPlaceholder !== ''"
-          v-text="inputPlaceholder"></span>
-      </div>
-    </template>
-
-    <template slot="right">
-      <!-- ------------ -->
-      <!--  CLEART BTN  -->
-      <!-- ------------ -->
       <div
         class="md-input-item-clear"
         v-if="clearable && !isDisabled && !readonly"
-        v-show="!isInputEmpty && isInputFocus"
+        v-show="!isInputEmpty"
         @click="$_clearInput"
       >
         <md-icon name="clear"></md-icon>
       </div>
+    </template>
 
-      <!-- ------------ -->
-      <!--  RIGHT SLOT  -->
-      <!-- ------------ -->
+    <template slot="right">
       <slot name="right"></slot>
     </template>
 
     <template slot="children">
-      <!-- -------------------- -->
-      <!-- BRIEF/ERROR TIP -->
-      <!-- -------------------- -->
       <div
-        v-if="isInputError()"
-        class="md-input-item-msg"
-      >
-        <p v-if="error !== ''" v-text="error"></p>
-        <slot name="error" v-else></slot>
-      </div>
-      <div
-        v-if="isInputBrief() && !isInputError()"
+        v-if="isInputBrief()"
         class="md-input-item-brief"
       >
-        <p v-if="brief !== ''" v-text="brief"></p>
+        <p v-if="brief !== ''" v-html="brief"></p>
         <slot name="brief" v-else></slot>
       </div>
-      <!-- ------------ -->
-      <!--   KEYBORARD  -->
-      <!-- ------------ -->
-      <md-number-keyboard
-        v-if="isVirtualKeyboard && !virtualKeyboardVm"
-        ref="number-keyboard"
-        :id="`${name}-number-keyboard`"
-        class="md-input-item-number-keyboard"
-        :ok-text="virtualKeyboardOkText"
-        :disorder="virtualKeyboardDisorder"
-      ></md-number-keyboard>
     </template>
   </md-field-item>
+  <div
+      v-if="isInputError()"
+      class="md-input-item-msg"
+    >
+    <p v-if="error !== ''" v-html="error"></p>
+    <slot name="error" v-else></slot>
+  </div>
+</div>
 </template>
 
-<script>import Icon from '../icon'
+<script>
+import Icon from '../icon'
 import FieldItem from '../field-item'
-import NumberKeyboard from '../number-keyboard'
 import {getCursorsPosition, setCursorsPosition} from './cursor'
 import {noop, isIOS, isAndroid, randomId} from '../_util'
 import {formatValueByGapRule, formatValueByGapStep, trimValue} from '../_util/formate-value'
 
 export default {
   name: 'md-input-item',
-
   components: {
     [Icon.name]: Icon,
-    [FieldItem.name]: FieldItem,
-    [NumberKeyboard.name]: NumberKeyboard,
+    [FieldItem.name]: FieldItem
   },
 
   inject: {
@@ -149,7 +107,7 @@ export default {
         return randomId('input-item')
       },
     },
-    title: {
+    label: {
       type: String,
       default: '',
     },
@@ -191,31 +149,17 @@ export default {
       type: Boolean,
       default: false,
     },
-    solid: {
-      type: Boolean,
-      default: true,
-    },
     clearable: {
       type: Boolean,
       default: false,
     },
-    isVirtualKeyboard: {
-      type: Boolean,
-      default: false,
+    imgSrc: {
+      type:  String,
+      default: '',
     },
-    virtualKeyboardDisorder: {
-      type: Boolean,
-    },
-    virtualKeyboardOkText: {
+    addon: {
       type: String,
-    },
-    virtualKeyboardVm: {
-      type: [Object, String],
-      default: null,
-    },
-    isTitleLatent: {
-      type: Boolean,
-      default: false,
+      default: '',
     },
     isFormative: {
       type: Boolean,
@@ -224,25 +168,20 @@ export default {
         return type === 'bankCard' || type === 'phone' || type === 'money' || type === 'digit'
       },
     },
-    isHighlight: {
-      type: Boolean,
-      default: false,
-    },
     isAmount: {
       type: Boolean,
       default: false,
     },
-    formation: {
-      type: Function,
-      default: noop,
-    },
+    reg:{
+      type: RegExp,
+      default: noop
+    }
   },
 
   data() {
     return {
       inputValue: '',
       inputBindValue: '',
-      inputNumberKeyboard: '',
       isInputFocus: false,
     }
   },
@@ -270,15 +209,11 @@ export default {
     inputMaxLength() {
       if (this.type === 'phone') {
         return 11
-      } else {
-        return this.maxlength
+      } else if(this.type==="bankCard") {
+        return 19
+      }else{
+           return this.maxlength
       }
-    },
-    inputPlaceholder() {
-      return this.isTitleLatent && this.isInputActive ? '' : this.placeholder
-    },
-    isInputActive() {
-      return !this.isInputEmpty || this.isInputFocus
     },
     isInputEmpty() {
       return !this.inputValue.length
@@ -286,6 +221,20 @@ export default {
     isDisabled() {
       return this.rootField.disabled || this.disabled
     },
+    unit: function() {
+						try {
+							var _REG = "千百十亿千百十万千百十元"
+							var num = Number(this.inputValue);
+              num = parseInt(num);
+							var temp = "";
+							for(var i = 0; i < num.toString().length; i++) {
+								temp += "" + _REG.split("").reverse().join("").charAt(i);
+							}
+							return temp.split("").reverse().join("").replace(/千百十亿千百十万千百十元$/, '千亿').replace(/百十亿千百十万千百十元$/, '百亿').replace(/十亿千百十万千百十元$/, '十亿').replace(/亿千百十万千百十元$/, '亿').replace(/千百十万千百十元$/, '千万').replace(/百十万千百十元$/, '百万').replace(/十万千百十元$/, '十万').replace(/万千百十元$/, '万').replace(/千百十元|百十元|十元|元$/, '');
+						} catch(e) {
+							return "";
+						}
+					}
   },
 
   watch: {
@@ -302,14 +251,9 @@ export default {
       this.$emit('change', this.name, val)
     },
     isInputFocus(val) {
-      if (!this.isVirtualKeyboard || !this.inputNumberKeyboard) {
-        return
-      }
       if (val) {
-        this.inputNumberKeyboard.show()
         this.$emit('focus', this.name)
       } else {
-        this.inputNumberKeyboard.hide()
         this.$emit('blur', this.name)
       }
     },
@@ -318,40 +262,40 @@ export default {
     this.inputValue = this.$_formateValue(this.$_subValue(this.value + '')).value
   },
   mounted() {
-    this.isVirtualKeyboard &&
-      this.$nextTick(() => {
-        this.$_initNumberKeyBoard()
-      })
-  },
-  beforeDestroy() {
-    const keyboard = this.inputNumberKeyboard
-    if (keyboard && keyboard.$el) {
-      document.body.removeChild(keyboard.$el)
-    }
   },
 
   methods: {
+     $_onClick(e) {
+      if (!this.disabled) {
+        this.$emit('click', e)
+      }
+    },
+    formation(name,curValue,curPos){
+      curValue = this.$_subValue(trimValue(curValue));
+      if(!this.reg.test(curValue)){
+        curValue = this.inputValue
+      }
+      return {value: curValue, range: curPos};
+    },
     // MARK: private methods
     $_formateValue(curValue, curPos = 0) {
       const type = this.type
       const name = this.name
       const oldValue = this.inputValue
       const isAdd = oldValue.length > curValue.length ? -1 : 1
-
       let formateValue = {value: curValue, range: curPos}
-
       // no format
-      if (!this.isFormative || curValue === '') {
+      if (!this.isFormative|| curValue === '') {
         return formateValue
       }
 
       // custom format by user
-      const customValue = this.formation(name, curValue, curPos)
-
-      if (customValue) {
-        return customValue
+      if(this.reg){
+        const customValue = this.formation(name, curValue, curPos);
+        if (customValue) {
+          return customValue
+        }
       }
-
       // default format by component
       let gap = ' '
       switch (type) {
@@ -360,7 +304,7 @@ export default {
           formateValue = formatValueByGapStep(4, curValue, gap, 'left', curPos, isAdd, oldValue)
           break
         case 'phone':
-          curValue = this.$_subValue(trimValue(curValue.replace(/\D/g, '')))
+          curValue = this.$_subValue(trimValue(curValue.replace(/^0{0,1}(1[3-9])[0-9]{9}$/, '')))
           formateValue = formatValueByGapRule('3|4|4', curValue, gap, curPos, isAdd)
           break
         case 'money':
@@ -387,7 +331,10 @@ export default {
           curValue = this.$_subValue(trimValue(curValue.replace(/\D/g, '')))
           formateValue.value = curValue
           break
-        /* istanbul ignore next */
+        case 'email':
+          /* istanbul ignore next */
+          curValue = this.$_subValue(trimValue(curValue.replace(/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, '')))
+          formateValue.value = curValue
         default:
           break
       }
@@ -412,50 +359,17 @@ export default {
       }
     },
     $_clearInput() {
-      this.inputValue = ''
-      !this.isTitleLatent && this.focus()
-    },
-    $_focusFakeInput() {
-      this.isInputFocus = true
-
-      setTimeout(() => {
-        this.$_addBlurListener()
-      }, 0)
-    },
-    $_blurFakeInput() {
-      this.isInputFocus = false
-      this.$_removeBlurListener()
-    },
-    $_addBlurListener() {
-      document.addEventListener('click', this.$_blurFakeInput, false)
-    },
-    $_removeBlurListener() {
-      document.removeEventListener('click', this.$_blurFakeInput, false)
-    },
-    $_initNumberKeyBoard() {
-      let keyboard =
-        (typeof this.virtualKeyboardVm === 'object'
-          ? this.virtualKeyboardVm
-          : this.$vnode.context.$refs[this.virtualKeyboardVm]) || this.$refs['number-keyboard']
-
-      if (Array.isArray(keyboard)) {
-        keyboard = keyboard[0]
-      }
-
-      keyboard.$on('enter', this.$_onNumberKeyBoardEnter)
-      keyboard.$on('delete', this.$_onNumberKeyBoardDelete)
-      keyboard.$on('confirm', this.$_onNumberKeyBoardConfirm)
-      this.inputNumberKeyboard = keyboard
-      document.body.appendChild(keyboard.$el)
+      this.inputValue = '';
+      this.focus()
     },
 
     // MARK: events handler
     $_onInput(event) {
+      this.isInputFocus = true
       const formateValue = this.$_formateValue(
         event.target.value,
         this.isFormative ? getCursorsPosition(event.target) : 0,
       )
-
       this.inputValue = formateValue.value
       this.inputBindValue = formateValue.value
 
@@ -464,6 +378,7 @@ export default {
           setCursorsPosition(event.target, formateValue.range)
         })
       }
+      this.$emit('change', this.name, event)
     },
     $_onKeyup(event) {
       this.$emit('keyup', this.name, event)
@@ -475,96 +390,78 @@ export default {
       this.$emit('keydown', this.name, event)
     },
     $_onFocus() {
-      this.isInputFocus = true
       this.$emit('focus', this.name)
     },
     $_onBlur() {
-      setTimeout(() => {
-        this.isInputFocus = false
         this.$emit('blur', this.name)
-      }, 100)
-    },
-    $_onFakeInputClick(event) {
-      if (this.isDisabled || this.readonly) {
-        return
-      }
-
-      this.$_blurFakeInput()
-
-      if (!this.isInputFocus) {
-        this.$_focusFakeInput(event)
-      }
-    },
-    $_onNumberKeyBoardEnter(val) {
-      this.inputValue = this.$_formateValue(this.inputValue + val).value
-    },
-    $_onNumberKeyBoardDelete() {
-      const inputValue = this.inputValue
-      if (inputValue === '') {
-        return
-      }
-      this.inputValue = this.$_formateValue(inputValue.substring(0, inputValue.length - 1)).value
-    },
-    $_onNumberKeyBoardConfirm() {
-      this.$emit('confirm', this.name, this.inputValue)
     },
 
     // MARK: public methods
     focus() {
-      if (this.isVirtualKeyboard) {
-        this.$_onFakeInputClick()
-      } else {
         this.$el.querySelector('.md-input-item-input').focus()
-        setTimeout(() => {
-          this.isInputFocus = true
-        }, 200)
-      }
     },
     blur() {
-      if (this.isVirtualKeyboard) {
-        this.$_blurFakeInput()
-      } else {
         this.$el.querySelector('.md-input-item-input').blur()
-        this.isInputFocus = false
-      }
+        //this.isInputFocus = false    
     },
     getValue() {
       return this.inputValue
     },
   },
 }
-</script>
+
+</script>
 
 <style lang="stylus">
+.content-wraper
+  background #ffffff
+  .unit-money
+    font-style normal
+    font-size 12px
+    color #B9B8B8
+    margin-left 56px
+    border-left 1px solid #B9B8B8
+    padding 5px 5px 3px
+.line-bottom
+    position relative
+    &::after
+      content ""
+      position absolute
+      bottom 0px
+      left 17px
+      right 0px
+      border-bottom 1px solid #eaeaea
+      transform scaleY(.5)
+      transform-origin 0 0  
 .md-input-item
-  .md-field-item-content
-    padding-top 0
-    padding-bottom 0
+  .md-field-item-title
+    width 25%
+    height 44px
+    line-height 44px
+    font-size 16px
+    color #333333
   .md-field-item-control
     display flex
     align-items center
 
 .md-input-item-clear
-  padding 10px 0
-  color input-item-icon
-  .md-icon
+    color #999
+    text-align center
+    margin-left 5px
+  .md-icon.icon-font.md
+    font-size 20px
     display flex
-    background color-bg-base
-    border-radius radius-circle
+    border-radius 50%
 
-.md-input-item-input,
-.md-input-item-fake
-  // display flex
+.md-input-item-input
   width 100%
-  height input-item-height
-  color input-item-color
-  font-size input-item-font-size
-  font-weight input-item-font-weight
-  font-family font-family-normal
-  line-height 1
+  line-height 22px
+  color #333333
+  font-size 16px
+  padding 13px 0
+  border-radius 3px
   -webkit-appearance none
   border none
-  background transparent
   outline none
   box-sizing border-box
   -webkit-tap-highlight-color transparent
@@ -574,33 +471,9 @@ export default {
   &:disabled, &[disabled]
     opacity 1
   &::-webkit-input-placeholder
-    color input-item-placeholder
-    font-weight font-weight-normal
+    color #C9C9C9
   &::-webkit-outer-spin-button, &::-webkit-inner-spin-button
     -webkit-appearance none
-
-.md-input-item-fake
-  line-height input-item-height
-  word-ellipsis()
-  cursor text
-  &::after
-    position relative
-    z-index 2
-    display none
-    content " "
-    height input-item-font-size
-    border-right solid 1.5px color-text-base
-    animation keyboard-cursor infinite 1s step-start
-  &.is-focus:after
-    display inline
-
-.md-input-item-fake-placeholder
-  position absolute
-  top 0
-  left 0
-  width 100%
-  color input-item-placeholder
-  font-weight font-weight-normal
 
 .md-input-item-msg,
 .md-input-item-brief
@@ -608,13 +481,11 @@ export default {
   &:not(:last-child)
     margin-bottom 10px
 
-.md-input-item-brief
-  font-size input-item-font-size-brief
-  color input-item-color-brief
-
 .md-input-item-msg
-  font-size input-item-font-size-error
-  color input-item-color-error
+  background #eaeaea
+  font-size 12px
+  color #F43F3F
+  padding: 5px 0 0 17px
 
 .md-input-item
   &.left
@@ -632,63 +503,28 @@ export default {
     .md-input-item-fake
       text-align right
 
-  &.is-title-latent
-    .md-field-item-title
-      position absolute
-      top 50%
-      left 0
-      height auto
-      font-size input-item-title-latent-font-size
-      color input-item-title-latent-color
-      transform translate3d(0, -50%, 0)
-      transition all .3s ease
-      opacity 0
-      will-change auto
-    .md-field-item-content
-      min-height 115px
-    .md-field-item-content,
-    .md-field-item-left,
-    .md-field-item-right,
-    .md-input-item-input,
-    .md-input-item-fake
-      padding-top 20px
-    &.is-active
-      .md-field-item-title
-        opacity 1
-        top 20px
-        transform translate3d(0, 0, 0)
-
-  &.is-highlight
-    &.is-focus
-      .md-field-item-content
-        hairline(bottom, input-item-color-highlight, 0, 4px)
 
   &.is-disabled
     .md-input-item-input,
-    .md-input-item-fake,
-    .md-input-item-fake-placeholder
-      -webkit-text-fill-color input-item-color-disabled
-      color input-item-color-disabled
+      -webkit-text-fill-color #898989
+      color #898989
 
-  &.is-amount
-    .md-input-item-input,
-    .md-input-item-fake
-      padding-top 5px
-      font-family font-family-number
-    &.large
-      .md-input-item-input,
-      .md-input-item-fake
-        padding-top v-gap-xs
 
   &.large
-    .md-input-item-input,
-    .md-input-item-fake
-      padding-bottom 15px
-      font-size input-item-font-size-large
+    .md-input-item-input
+      font-size 32px
+      line-height 32px
+      height 42px
     .md-input-item-input::-webkit-input-placeholder
-        font-size 60px
-        line-height 100px
-
+        font-size 16px
+    .md-field-item-left 
+        position relative
+        font-size 30px
+        
+    .md-field-item-content
+        height 48px
+        left -7px
+        padding-right 10px !important
   &.is-error
     .md-field-item-content
       hairline(bottom, input-item-color-error, 0, 4px)
@@ -696,7 +532,6 @@ export default {
   &.is-ios
     .md-input-item-input::-webkit-input-placeholder
       position relative
-      top 3px
       overflow visible
     .md-input-item-fake::after
       border-right solid 6px #2C6CF5
