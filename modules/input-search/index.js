22d08186cfb9fda8db68f5baf994d45d54586c8c
taiyang1994 <1209450705@qq.com>
2019-12-20 16:45:54 +0800

    金额输入框小数点的问题

--- modules/input-search/index.js
+++ modules/input-search/index.js
@@ -2,4 +2,4 @@ import search from '../../components/input-search/index';
 search.install = function (Vue) {
   Vue.component(search.name, search);
 };
-export default inputSearch;
+export default search;
